9e5b062bf4ed6208520820e6ff965b05877ddc59
cd <coraldarling@outlook.com>
2018-10-07 15:35:34 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -52,7 +52,7 @@ def checkout(cart, coupons)
   total_cost = 0
   #binding.pry
     updated_cart = consolidate_cart(cart)
-    binding.pry
+    
     cart_w_coupons = apply_coupons(updated_cart, coupons)
     final_cart = apply_clearance(cart_w_coupons)
   binding.pry
