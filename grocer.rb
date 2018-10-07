7f89b26e8b16455c7afc6b78265156732b8739a8
cd <coraldarling@outlook.com>
2018-10-07 13:28:48 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,81 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  proper_cart = {}
+  cart.each do |item_hash| 
+    item_hash.each do |name, info| 
+      if !proper_cart[name]
+        proper_cart[name] = {}
+      end
+      if !proper_cart[name][info] 
+        proper_cart[name] = info
+      end
+      if !proper_cart[name][:count]
+        proper_cart[name][:count] = 1
+      else
+        proper_cart[name][:count] += 1
+      end
+    end   
+  end
+  proper_cart
 end
 
-def apply_coupons(cart, coupons)
-  # code here
+def apply_coupons(cart, coupons) 
+  updated_cart = {}
+  #item = coupon[:item]
+  cart.each do |item_name, item_info|
+    coupons.each do |coupon|
+      updated_cart["#{item_name} W/COUPON"] = {}
+      updated_cart[item_name] = item_info
+       
+      #binding.pry
+        if !coupon || !coupon.values.include?(item_name)
+          updated_cart
+        end
+        if updated_cart.include?(coupon[:item]) && coupon[:num] <= updated_cart[item_name][:count]
+          #binding.pry
+          quantity = updated_cart[coupon[:item]][:count] / coupon[:num]
+          multi_quanity = updated_cart[coupon[:item]][:count] % coupon[:num]
+          if updated_cart["#{coupon[:item]} W/COUPON"][:count]
+            updated_cart["#{coupon[:item]} W/COUPON"][:count] += 1
+            
+        #binding.pry
+          elsif 
+            updated_cart[coupon[:item]][:count] /= coupon[:num]
+            updated_cart["#{coupon[:item]} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[item_name][:clearance], :count => quantity}
+            updated_cart[item_name][:count] = multi_quanity 
+          else 
+            !coupon || !coupon.values.include?(item_name)
+          updated_cart
+            
+        end
+      end
+    end
+  end
+  updated_cart
+  #binding.pry
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, item_info|
+    if item_info[:clearance] == true
+      item_info[:price] = (item_info[:price] * 0.8).round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  total_cost = 0
+  updated_cart = consolidate_cart(cart)
+  apply_coupons(updated_cart, coupons)
+  apply_clearance(updated_cart)
+  updated_cart.each do |name,info|
+    total_cost = updated_cart[info][:price] * updated_cart[info][:count]
+    if total_cost > 100
+      total_cost *= 0.9
+    end
+  end
+  total_cost
+  #binding.pry
 end
