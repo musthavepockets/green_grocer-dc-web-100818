dec07a680a5caab289ac7138adba366cdd041b55
cd <coraldarling@outlook.com>
2018-10-07 15:36:36 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -50,12 +50,10 @@ end
 
 def checkout(cart, coupons)
   total_cost = 0
-  #binding.pry
     updated_cart = consolidate_cart(cart)
     
     cart_w_coupons = apply_coupons(updated_cart, coupons)
     final_cart = apply_clearance(cart_w_coupons)
-  #binding.pry
     
   final_cart.each do |name,info|
     total_cost += info[:price] * info[:count]
